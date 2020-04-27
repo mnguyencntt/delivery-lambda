772c359e94024f679a01979e488598f16832b11f
MinhNguyen <MinhNguyen>
2020-04-27 23:39:53 +0800

    update BaseTest

--- src/test/java/com/anz/platform/service/DeliveryServiceTest.java
+++ src/test/java/com/anz/platform/service/DeliveryServiceTest.java
@@ -1,50 +1,16 @@
 package com.anz.platform.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
-import org.apache.commons.dbutils.DbUtils;
-import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.junit.jupiter.api.Test;
 import com.anz.platform.base.BaseTest;
 import com.anz.platform.config.AppConfigMock;
 import com.anz.platform.domain.DbInfo;
 import com.anz.platform.model.Delivery;
-import com.anz.platform.util.ObjectUtils;
 
 public class DeliveryServiceTest extends BaseTest {
   private final DbInfo dbInfo = new AppConfigMock().getDbInfo();
   private final DeliveryService deliveryService = new DeliveryService(dbInfo);
-
-  @Test
-  public void loadSqlFile() throws Exception {
-    File file = getFileFromResources("file/delivery.sql");
-    String printFile = printFile(file);
-    // System.out.println(printFile);
-    assertTrue(ObjectUtils.isNotEmpty(printFile));
-  }
-
-  @Test
-  public void testUserDbConnection() throws SQLException, ClassNotFoundException {
-    final DbInfo dbInfo = new AppConfigMock().getDbInfo();
-
-    Class.forName(dbInfo.getSqlDriver());
-    final QueryRunner run = new QueryRunner();
-    final Connection conn = DriverManager.getConnection(dbInfo.getEndpoint(), dbInfo.getUsername(), dbInfo.getPassword());
-    try {
-      List<Map<String, Object>> maps = run.query(conn, "SELECT * FROM DELIVERY", new MapListHandler());
-      // System.out.println(maps);
-      assertEquals(1, maps.size());
-    } finally {
-      DbUtils.close(conn);
-    }
-  }
 
   @Test
   public void testFindAllDeliveries() {
